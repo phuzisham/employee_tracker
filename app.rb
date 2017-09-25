9a769933ca88fdb055f063b93194e3eb075168c4
David Hale and Margaret Berry <margaret@>
2017-09-25 14:11:15 -0700

    add styling

--- app.rb
+++ app.rb
@@ -15,8 +15,7 @@ end
 post("/create_division") do
   description = params["description"]
   Division.create({:description => description})
-  @divisions = Division.all()
-  erb(:index)
+  redirect "/"
 end
 
 get("/division/:id") do
@@ -30,4 +29,35 @@ post("/division/:id") do
   name = params['name']
   Employee.create({:name => name, :division_id => division_id})
   erb(:division)
+end
+
+patch("/division/:id") do
+  @division = Division.find(params["id"])
+  description = params["description"]
+  @division.update({:description => description})
+  erb(:division)
+end
+
+delete("/division/:id") do
+  @division = Division.find(params["id"])
+  @division.delete
+  redirect "/"
+end
+
+get("/employee/:id") do
+  @employee = Employee.find(params["id"])
+  erb(:employee)
+end
+
+patch("/employee/:id") do
+  @employee = Employee.find(params["id"])
+  name = params["name"]
+  @employee.update({:name => name})
+  erb(:employee)
+end
+
+delete("/employee/:id") do
+  @employee = Employee.find(params["id"])
+  @employee.delete
+  redirect "/"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,16 +8,16 @@ ENV['RACK_ENV'] = 'test'
 
 RSpec.configure do |config|
   config.after(:each) do
-    Employee.all().each() do |task|
-      task.destroy()
+    Employee.all().each() do |employee|
+      employee.destroy()
     end
   end
 end
 
 RSpec.configure do |config|
   config.after(:each) do
-    Division.all().each() do |task|
-      task.destroy()
+    Division.all().each() do |division|
+      division.destroy()
     end
   end
 end
